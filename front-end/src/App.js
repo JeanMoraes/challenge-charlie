df52242db5162572d48246f69aa640c7d61da0ca
Jean Moraes <moraes.jean91@gmail.com>
2020-08-13 11:49:26 -0300

    add function getUserLocation

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  useEffect(()=>{
+    getUserLocation();
+  }, []);
+
+  async function getUserLocation(){
+    navigator.geolocation.getCurrentPosition( async function(position) {
+      const userPosition = {lat: position.coords.latitude, lng: position.coords.longitude};
+      console.log(userPosition);
+    });
+  }
+
   return (
     <div className="App">
       CHALLENGE CHARLIE
